3ff32d47337459cd89ab72243ed5507c9d20538d
Josh Crowder <josh.crowder@crowdstrike.com>
2017-12-14 14:06:05 +0000

    New settings

--- test.zsh
+++ test.zsh
@@ -1,4 +1,6 @@
-lw=('environment' 'terminal' 'editor' 'history' 'directory' 'spectrum' 'utility' 'completion' 'prompt' 'ssh' 'homebrew' 'osx' 'ruby' 'rails' 'node' 'git' 'syntax-highlighting' 'history-substring-search' 'prompt')
+#!/usr/bin/env zsh
+
+lw=('environment' 'terminal' 'editor' 'history' 'directory' 'spectrum' 'utility' 'completion' 'prompt' 'ssh' 'homebrew' 'osx' 'ruby' 'rails' 'node' 'git' 'syntax-highlighting' 'history-substring-search')
 
 for i in $lw; do
   echo $i
